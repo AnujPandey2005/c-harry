d80ad7331c6ce67bdc61a2f27e85dddabde02f2e
anujpandey <ap9089511@gmail.com>
2025-04-16 13:58:26 +0530

    change

--- tut3.cpp
+++ tut3.cpp
@@ -8,9 +8,7 @@ int main()
     // int b= 15;
     int a=14,b=15;
     float pi=3.15; 
-    double ci=80.90987665;
     cout<<"this is tutorial 3 .\n here the value of a is \n"<<a<<". the value of b is \n"<<b;
     cout<<" the value of pi is "<<pi;
-    cout<<"the value off ci is"<<ci;
     return 0;
 }
