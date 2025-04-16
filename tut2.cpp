036bc64cadd5ddea396204defef987f1c25d6eb6
anujpandey <ap9089511@gmail.com>
2025-04-16 13:56:31 +0530

    message

---
+++ tut2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+/*
+this code
+is written 
+by mr 
+anuj
+pandey*/
+int main()
+{
+    int sum=10;
+    cout<<"hello world"<<sum;
+    return 0;
+}
---
+++ tut3.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    // int a= 14;
+    // int b= 15;
+    int a=14,b=15;
+    float pi=3.15; 
+    double ci=80.90987665;
+    cout<<"this is tutorial 3 .\n here the value of a is \n"<<a<<". the value of b is \n"<<b;
+    cout<<" the value of pi is "<<pi;
+    cout<<"the value off ci is"<<ci;
+    return 0;
+}
