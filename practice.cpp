7ee5dbbc905164c9d1f8f95d7ebe87d24110fda2
anujpandey <ap9089511@gmail.com>
2025-04-28 20:53:37 +0530

    changes

---
+++ practice.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a =19,b=18;
+    float pi=3.14;
+    double d=3.9089765;
+    cout  <<"this is a practice file .\n here the value of a is "<<a<<". \n the value of b is "<<b<<".\n the value of pi is "<<pi<<".\n the value of d is "<<d<<endl;
+}
